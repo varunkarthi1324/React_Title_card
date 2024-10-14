d27b2e2a36086ad66392d1894ae815fd4a900bf7
varunkarthi1324 <vkvarunkarthikeyan@gmail.com>
2024-10-15 02:50:34 +0530

    this is a simple name card

---
+++ src/index.js
@@ -0,0 +1,100 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <Skillist />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="image.png" alt="Tom &jerry" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Varun Karthikeyan M</h1>
+      <p>
+        Hello, I'm Varun Karthikeyan, a web developer skilled in building
+        dynamic, user-friendly websites using technologies like HTML, CSS,
+        JavaScript, React, Node.js, and Express. I focus on creating visually
+        appealing, high-performance sites and am always eager to learn and
+        improve.
+      </p>
+    </div>
+  );
+}
+function Skillist() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </div>
+  );
+}
+function Skill({ skill, color, level }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
